ee4c2d57d0adeea507e45a8c3d7b3a3c5d0d7049
vlada-j <vladajanosevic@gmail.com>
2019-06-05 04:01:12 +0200

    Add current/old site

--- app/index.vue
+++ app/index.vue
@@ -3,11 +3,9 @@
 		<nav class="nav fixed">
 			<a v-for="link in links" :href="'#' + link.link" class="btn">{{link.label}}</a>
 		</nav>
-		<hr>
 		<keep-alive>
 			<component :is="rootScope.currentView" :root-scope="rootScope"></component>
 		</keep-alive>
-		<hr>
 	</section>
 </template>
 
@@ -17,7 +15,8 @@ exports = {
 		{ label:'Converter',		link:'converter' },
 		{ label:'Testing',			link:'testing' },
 		{ label:'JS Console',		link:'js-console' },
-		{ label:'Image convert',	link:'image-convert' }
+		{ label:'Image convert',	link:'image-convert' },
+		{ label:'Andrej',	link:'andrej' }
 	],
 	props: ['rootScope'],
 	currentView: 'home'
